1e0ca0d5c0310a3d729f232ff37963b70d566d2b
sseongeun <96720963+sseongeun@users.noreply.github.com>
2024-02-04 23:24:14 +0900

    [Bronze I] Title: 암호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1855. 암호/암호.py
@@ -0,0 +1,16 @@
+n = int(input())
+s = input()
+arr = []
+for i in range(0, len(s), n):
+    arr.append(list(s[i:i+n]))
+
+for i in range(len(arr)):
+    if i % 2 != 0:
+        data = list(reversed(arr[i]))
+        arr[i] = data
+
+res = ''
+for j in range(n):
+    for i in range(len(arr)):
+        res += arr[i][j]
+print(res)
