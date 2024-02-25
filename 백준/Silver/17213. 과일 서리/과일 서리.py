0d89144700df4950d858867b8dc9a9a553dc6221
sseongeun <96720963+sseongeun@users.noreply.github.com>
2024-02-25 20:26:24 +0900

    [Silver II] Title: 과일 서리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/17213. 과일 서리/과일 서리.py
+++ 백준/Silver/17213. 과일 서리/과일 서리.py
@@ -1,7 +1,11 @@
-from math import comb
+def factorial(num):
+    if(num>1):
+        return num*factorial(num-1)
+    else:
+        return 1
+    
 N=int(input())
 M=int(input())
-
-
-
-print(comb(M-1,M-N))
+top=factorial(M-1)
+bottom=factorial(M-N)*factorial(N-1)
+print(top//bottom)
