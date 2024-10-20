751bc2d29978de6947a74398c25a9246eb8aa470
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2024-10-20 16:17:58 +0900

    [level 2] Title: 짝지어 제거하기, Time: 150.31 ms, Memory: 20 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,16 @@
+def solution(s):
+    s=list(s)
+    stack=[]
+    for i in s:
+        if len(stack)==0:
+            stack.append(i)
+        elif stack[-1]==i:
+            stack.pop()
+        else:
+            stack.append(i)
+            
+            
+    if len(stack)==0:
+        return 1
+    else:
+        return 0
