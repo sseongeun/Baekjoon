7d6304008dd7c3304bc103e069c4dcdf95281928
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2024-12-23 02:23:14 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,13 +1,13 @@
 def solution(num):
-    count=0
-    while(num!=1 and count<500):
-        if(num%2==0):
-            num=num//2
-            count+=1
+    answer =0
+    while (num!=1 and answer <=500):
+        if num%2==0:
+           num/=2
+           answer+=1
         else:
-            num=num*3+1
-            count+=1
-    if(count>=500):
+         num=(num*3+1)
+         answer+=1
+    if answer>500:
         return -1
-    else:
-        return count
+    return answer
+    
