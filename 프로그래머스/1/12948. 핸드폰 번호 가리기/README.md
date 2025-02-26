6c955442c1bbda955e396a365224d944ee4d2680
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-02-26 18:35:46 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.78 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/README.md
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 핸드폰 번호 가리기 - 12948 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 75.7 MB, 시간: 1.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 31일 15:50:50
+2025년 02월 26일 18:35:44
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        int len = phone_number.length();
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i =0;i<len-4;i++){
+            sb.append("*");
+        }
+        answer = sb.toString()+phone_number.substring(len-4,len);
+        return answer;
+        
+        
+    }
+}
