8308e2e29a777367e48f7148a2b51a4ae4d3d2ba
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-03-12 17:03:14 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,8 +1,9 @@
 import math
 def solution(n):
-    answer=-1
-    for i in range(int(math.sqrt(n))+1,-1,-1):
-        if(n==i**2):
-            answer=(i+1)**2
-            break 
-    return answer
+    answer = -1
+    for i in range(int(math.sqrt(n)),-1,-1):
+        if i**2 == n:
+            return (i+1)**2
+        else:
+            return answer
+      
