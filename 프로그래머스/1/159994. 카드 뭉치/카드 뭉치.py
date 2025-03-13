95eb869542bda43acc71f7ebf9eb66a71f1e9f00
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-03-13 17:27:54 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,15 +1,11 @@
 def solution(cards1, cards2, goal):
-
-    cards1.reverse()
-    cards2.reverse()
-    
+    answer = "Yes"
     for i in goal:
-        
-        if len(cards1)!=0 and i == cards1[-1]:
-            cards1.pop()
-        elif  len(cards2)!=0 and i == cards2[-1] :
-            cards2.pop()
+        if len(cards1)!=0 and i == cards1[0]:
+            cards1.pop(0)
+        elif len(cards2)!=0 and i == cards2[0] :
+            cards2.pop(0)
         else:
             return "No"
-        
-    return "Yes"
+    return answer
+   
