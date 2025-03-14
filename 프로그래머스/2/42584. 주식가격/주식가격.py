3f63cb6cc9ec883af8eac6c58a0b0e5919e949a2
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-03-14 17:08:30 +0900

    [level 2] Title: 주식가격, Time: 153.56 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,18 +1,14 @@
 from collections import deque
 
 def solution(prices):
-    answer = []
-    
-    queue=deque(prices)
-    
-    while queue:
-        first = queue.popleft()
+    answer=[]
+    for i in range(len(prices)):
         cnt=0
-        for i in queue:
-            cnt+=1
-            if first>i:
+        for j in range(i+1,len(prices)):
+            if prices[i]>prices[j]:
+                cnt+=1
                 break
+            cnt+=1
         answer.append(cnt)
-                
-    
+
     return answer
