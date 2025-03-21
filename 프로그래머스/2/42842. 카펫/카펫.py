f1c01cf2afa3fca8e160d88e8dfc5016efe5581e
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-03-21 20:41:23 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,10 @@
 import math
 
 def solution(brown, yellow):
-    
-    for i in range(1,yellow+1):
-        if(yellow % i ==0):
-            yellow_row=yellow//i
-            yellow_col=i
-            if (2*yellow_row+2*yellow_col+4)==brown:
-                return [yellow_row+2,yellow_col+2] 
+    for i in range(1,int(math.sqrt(yellow))+1):
+        if yellow%i==0:
+            saro=i
+            garo=yellow//i
+
+            if (2*(saro+garo+2))==brown:
+                return [garo+2,saro+2]
