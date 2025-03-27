3166587799cc6c2a1aed8cd6ae71b0ef7a60f4d0
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-03-27 21:55:54 +0900

    [level 2] Title: 더 맵게, Time: 1917.39 ms, Memory: 50.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,20 +1,24 @@
 import heapq
+
 def solution(scoville, K):
-    answer = 0
+    scoville.sort()
     heapq.heapify(scoville)
+    cnt=0
+    
     
     while True:
         minNum = heapq.heappop(scoville)
+
         if minNum >=K:
             break
+
         elif minNum < K and len(scoville)==0:
-            answer=-1
+            cnt=-1
             break
+
         else:
-            secondMinNum = heapq.heappop(scoville)
-            heapq.heappush(scoville,minNum+(secondMinNum*2))
-            answer+=1
-    return answer
-    
-    
-    
+            nextNum = heapq.heappop(scoville)
+            heapq.heappush(scoville, minNum+(nextNum*2))
+            cnt+=1
+
+    return cnt
