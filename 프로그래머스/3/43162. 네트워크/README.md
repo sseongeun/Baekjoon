5bfb3b8361a4efb24f9f3c871f8309f6d506f523
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-03-28 20:26:26 +0900

    [level 3] Title: 네트워크, Time: 1.54 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 9.36 MB, 시간: 0.57 ms
+메모리: 9.23 MB, 시간: 1.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 25일 20:55:11
+2025년 03월 28일 20:26:24
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,33 @@
-def dfs(computers,n,start,visited):
-    if visited[start]==False:
-        visited[start]=True
-        for i in range(n):
-            if computers[start][i]==1 and visited[i]== False:
-                dfs(computers,n,i,visited)
-    else:
-        return
+def dfs(visited,graph,curr):
+    queue=[]
+    queue.append(curr)
+    visited[curr]=True
+    
+    while queue:
+        curr = queue.pop()
+        
+        for neighbors in graph[curr]:
+            if visited[neighbors]==False:
+                queue.append(neighbors)
+                visited[neighbors]=True
+    
 
 def solution(n, computers):
+    graph=[[] for _ in range(n)]
     visited=[False]*n
-    cnt=0
-    
+
+    for i in range(n):
+        for j in range(n):
+            if i!=j and computers[i][j]==1:
+                graph[i].append(j)
+                graph[j].append(i)
+                graph[i]=list(set(graph[i]))
+                graph[j]=list(set(graph[j]))
+
+    cnt=0           
     for i in range(n):
         if visited[i]==False:
-            dfs(computers,n,i,visited)
+            dfs(visited,graph,i)
             cnt+=1
+
     return cnt
