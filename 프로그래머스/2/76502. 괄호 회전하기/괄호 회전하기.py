89680a1214a014e890e497e8be505d079671d42d
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-03-28 22:16:15 +0900

    [level 2] Title: 괄호 회전하기, Time: 162.86 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,29 +1,29 @@
+def check_string(s):
+    stack=[]
+    for i in s:
+        if i == '[' or i=='{' or i =='(':
+            stack.append(i)
+        else:
+            if i==']':
+                if stack and stack[-1]=='[': stack.pop() 
+                else : stack.append(i)
+            elif i=='}':
+                if stack and stack[-1]=='{': stack.pop() 
+                else : stack.append(i)
+            else:
+                if stack and stack[-1]=='(': stack.pop() 
+                else : stack.append(i)
+                
+    if len(stack)==0 :return 1
+    else: return 0
+
 def solution(s):
-    key_list=['[',']','{','}','(',')']
-    value_list=[1,-1,2,-2,3,-3]
-    dic=dict(zip(key_list,value_list))
+    s=list(s)
+    s_len=len(s)
+    s=s*2
+    answer=0
+    for i in range(s_len):
+        temp =s[i:i+s_len]
+        answer+=check_string(temp)
 
-    turn =len(s)
-    s = s*2 
-    
-    result=0
-    for start in range(turn):
-        stack = []
-        prev=0
-        for i in range(start,start+turn):
-
-            if prev == -dic[s[i]]  and prev>dic[s[i]]:
-                stack.pop()
-            else:
-                stack.append(dic[s[i]])
-                prev= dic[s[i]]
-
-
-            if stack: 
-                prev=stack[-1]
-            else:
-                prev =0
-        if len(stack)==0:
-            result+=1
-    
-    return result
+    return answer
