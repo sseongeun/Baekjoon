5146f6f9e59cfa1b1dd812d382a6763a88276d3e
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-03-31 00:36:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.05 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,5 +1,8 @@
 from collections import Counter
+
+
+
 def solution(participant, completion):
-    
-    diff = Counter(participant)-Counter(completion)
-    return list(diff.elements())[0]
+
+    counter = Counter(participant)-Counter(completion)
+    return ''.join(list(counter))
