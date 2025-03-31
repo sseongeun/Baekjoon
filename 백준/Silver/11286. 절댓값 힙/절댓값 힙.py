c5b08411a50127fcdaa506ecc15edc9d642221c7
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-03-31 17:19:53 +0900

    [Silver I] Title: 절댓값 힙, Time: 136 ms, Memory: 41120 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,26 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+N=int(input())
+num_list=[]
+heapq.heapify(num_list)
+
+for i in range(N):
+    input_num = int(input())
+    
+    if input_num == 0 and len(num_list)==0:
+        print("0")
+        
+    elif input_num == 0:
+        curr = heapq.heappop(num_list)
+        if curr[1]==-1:
+            print((-1)* curr[0])
+        else:
+            print(curr[0])
+
+    else:
+        if input_num<0:
+            heapq.heappush(num_list,(abs(input_num),-1))
+        else:
+            heapq.heappush(num_list,(input_num,1))
