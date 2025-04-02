8ac26d03374af019d37331b30755fd09be9a1f2b
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-04-02 17:05:32 +0900

    [Silver III] Title: 수열, Time: 76 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,13 +1,16 @@
-import sys
-input=sys.stdin.readline
+N,K=map(int,input().split()) # 수의 개수, 연속된 일자수
+nums = list(map(int,input().split()))
 
-N,K=map(int,input().split())
-L=list(map(int,input().split()))
-result=[]
-curr=sum(L[0:K])
-result.append(curr)
-for i in range(0,N-K):
-    curr=curr-L[i]+L[i+K]
-    result.append(curr)
+start =0
+end = K-1
+curr_sum = sum(nums[start:end+1])
+max_nums=[]
+max_nums.append(curr_sum)
 
-print(max(result))
+while end<N-1:
+    curr_sum = curr_sum-nums[start]+nums[end+1]
+    max_nums.append(curr_sum)
+    start+=1
+    end+=1
+    
+print(max(max_nums))
