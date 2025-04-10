6ea27011136720cd0310b698aa78dda2586ccce6
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-04-10 15:44:40 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,37 +1,33 @@
 from collections import deque
 
-
-row,col=map(int,input().split())
-graph=[]
-for i in range(row):
-    graph.append(list(map(int,input())))
-    
-def dns(graph,row,col):
+def dfs(graph,visited,N,M):
+ 
     queue=deque()
     queue.append((0,0))
+    visited[0][0]=True
     
-    dx=[0,0,-1,1]
-    dy=[-1,1,0,0]
+    dx=[-1,1,0,0]
+    dy=[0,0,-1,1]
     
     while queue:
         x,y=queue.popleft()
         
         for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
+            nx,ny=x+dx[i],y+dy[i]
             
-            if nx<0 or nx>=row or ny<0 or ny>=col:
-                continue
-            
-            if graph[nx][ny] == 0:
-                continue
-            
-            if graph[nx][ny] == 1:
+            if 0<=nx<N and 0<=ny<M and visited[nx][ny]==False and graph[nx][ny]==1:
                 queue.append((nx,ny))
-                graph[nx][ny]=graph[x][y]+1
-        
-    return graph[-1][-1]
+                graph[nx][ny]= graph[x][y]+1
+                visited[nx][ny]=True
+     
+    return graph[N-1][M-1]
+    
+N,M=map(int,input().split())
 
-
-
-print(dns(graph,row,col))
+graph=[]
+for _ in range(N):
+    graph.append(list(map(int,input())))
+    
+visited=[[False for _ in range(M)] for _ in range(N)]
+    
+print(dfs(graph,visited,N,M))
