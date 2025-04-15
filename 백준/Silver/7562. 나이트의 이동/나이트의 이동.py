3d036a55e312a4f25d50eadcbf9ee49ddfdb4536
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-04-15 21:28:38 +0900

    [Silver I] Title: 나이트의 이동, Time: 1120 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,34 +1,33 @@
 from collections import deque
-def bfs(startX,startY,endX,endY,L,graph):
-    if startX==endX and startY==endY:
+import sys
+input = sys.stdin.readline
+
+def dfs(graph,start,end,N):
+    if start == end :
         return 0
     
-    dx=[1,2,2,1,-1,-2,-2,-1]
-    dy=[-2,-1,1,2,-2,-1,1,2]
+    dx=[-2,-1,1,2,-2,-1,1,2]
+    dy=[1,2,2,1,-1,-2,-2,-1]
     
-    q=deque()
-    q.append((startX,startY))
+    queue=deque()
+    queue.append((start[0],start[1]))
     
-    while q:
-        x,y=q.popleft()
-    
+    while queue:
+        x,y= queue.popleft()
         for i in range(8):
-            nx=x+dx[i]
-            ny=y+dy[i]
+            nx,ny=x+dx[i],y+dy[i]
             
-            if nx<0 or nx>=L or ny<0 or ny>=L:
-                continue
-            
-            if graph[nx][ny]==0:
-                q.append((nx,ny))
-                graph[nx][ny]=graph[x][y]+1
-    
-    return graph[endX][endY]
+            if 0<=nx<N and 0<=ny<N and graph[nx][ny]==0:
+                graph[nx][ny]= graph[x][y]+1
+                queue.append((nx,ny))
+                if nx==end[0] and ny==end[1]:
+                    return graph[nx][ny]
+                
 
 T=int(input())
 for _ in range(T):
-    L=int(input()) # 한변의 길이
-    graph=[[0]*L for _ in range(L)]
-    startX, startY = map(int,input().split())
-    endX , endY = map(int,input().split())
-    print(bfs(startX,startY,endX,endY,L,graph))
+    N=int(input())
+    graph=[[0 for _ in range(N)] for _ in range(N)]
+    start=list(map(int,input().split()))
+    end=list(map(int,input().split()))
+    print(dfs(graph,start,end,N))
