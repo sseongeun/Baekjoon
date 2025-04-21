01617f032d1af508083cd2d42708f90ef072317e
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-04-21 17:30:57 +0900

    [Silver III] Title: 조합, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.py
+++ 백준/Silver/2407. 조합/조합.py
@@ -1,9 +1,12 @@
-def fac(num):
-    if (num>1):
-        return num*fac(num-1)
-    else:
-        return 1
-
-
-n,m=map(int,input().split())
-print(fac(n)//(fac(m)*fac(n-m)))
+N,M=map(int,input().split())
+dic={}
+for i in range(N+1):
+    for j in range(i+1):
+        if j==0:
+            dic[(i,j)]=1
+        elif j==i:
+            dic[(i,j)]=1
+        else:
+            dic[(i,j)]= dic[(i-1,j-1)]+ dic[(i-1,j)]
+            
+print(dic[(N,M)])
