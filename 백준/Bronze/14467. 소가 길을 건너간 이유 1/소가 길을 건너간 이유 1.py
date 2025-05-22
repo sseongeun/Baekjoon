cefde70309b3a4abeb8a45e2ff059e1db394dee1
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2025-05-23 00:36:01 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
@@ -1,16 +1,13 @@
-dic={}
+N=int(input())
 cnt=0
+dic={}
 
-n= int(input())
-for _ in range(n):
-    cow_num, location = map(int,input().split())
-    if cow_num not in dic.keys(): # 이전 내역이 없는 경우
-        dic[cow_num]=location
-    else: # 이전 내역이 있는 경우
-        prev=dic[cow_num]
-        if location != prev:
-            cnt+=1
-            dic[cow_num]=location
-            
+for _ in range(N):
+    num,loc = map(int,input().split())
+    if num not in dic.keys():
+        dic[num]=loc
+    elif loc != dic[num]:
+        cnt+=1
+        dic[num]=loc
+        
 print(cnt)
-            
